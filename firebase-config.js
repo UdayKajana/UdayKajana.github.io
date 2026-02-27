357863b22f028f9c2347fb3a616f7166bdf0a4c3
uday <kajanauday@gmail.com>
2026-02-27 21:25:52 +0530

    fix: API issue

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCBH192yfKBuy_9P3NcqLtvQMt5XN7Fofk",
+    authDomain: "udaykajananotes.firebaseapp.com",
+    databaseURL: "https://udaykajananotes-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "udaykajananotes",
+    storageBucket: "udaykajananotes.firebasestorage.app",
+    messagingSenderId: "128489649264",
+    appId: "1:128489649264:web:4a4c704c746ffa5500b0b5",
+    measurementId: "G-L313DMGD6Z"
+};
